fcee85a370577b41c950634b40d9fa465e57879b
roma <romatorlopov7@gmail.com>
2025-11-10 20:32:06 +0300

    4. Ставим фронт админки, установка Admin Lte

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'Main'], function(){
+Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
+});
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
 });
 
 Auth::routes();
