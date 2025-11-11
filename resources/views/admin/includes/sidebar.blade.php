a56cac6c8c9932e0efb344538678dd0d49f5cda2
Kirill <ignatovkirill5@gmail.com>
2025-11-11 18:57:26 +0300

    13. Полностью добавляем CRUD для тегов

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,34 +1,33 @@
       <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
-        <!--begin::Sidebar Brand-->
-        <div class="sidebar-brand">
-          <!--begin::Brand Link-->
-          <a href="#" class="brand-link">
-            <!--begin::Brand Image-->
-            <h1>Blog</h1>
-          </a>
-          <!--end::Brand Link-->
-        </div>
-        <div class="sidebar-wrapper">
-          <nav class="mt-2">
-            <!--begin::Sidebar Menu-->
-            <ul
-              class="nav sidebar-menu flex-column"
-              data-lte-toggle="treeview"
-              role="navigation"
-              aria-label="Main navigation"
-              data-accordion="false"
-              id="navigation"
-            >
-              <li class="nav-item">
-                <a href="{{ route('admin.category.index') }}" class="nav-link">
-                  <i class="nav-icon"></i>
-                  <p>Категории</p>
-                </a>
-              </li>
-
-            </ul>
-            <!--end::Sidebar Menu-->
-          </nav>
-        </div>
-        <!--end::Sidebar Wrapper-->
+          <!--begin::Sidebar Brand-->
+          <div class="sidebar-brand">
+              <!--begin::Brand Link-->
+              <a href="#" class="brand-link">
+                  <!--begin::Brand Image-->
+                  <h1>Blog</h1>
+              </a>
+              <!--end::Brand Link-->
+          </div>
+          <div class="sidebar-wrapper">
+              <nav class="mt-2">
+                  <!--begin::Sidebar Menu-->
+                  <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="navigation"
+                      aria-label="Main navigation" data-accordion="false" id="navigation">
+                      <li class="nav-item">
+                          <a href="{{ route('admin.category.index') }}" class="nav-link">
+                              <i class="nav-icon bi bi-list"></i>
+                              <p>Категории</p>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="{{ route('admin.tag.index') }}" class="nav-link">
+                              <i class="nav-icon bi bi-tag"></i>
+                              <p>Теги</p>
+                          </a>
+                      </li>
+                  </ul>
+                  <!--end::Sidebar Menu-->
+              </nav>
+          </div>
+          <!--end::Sidebar Wrapper-->
       </aside>
